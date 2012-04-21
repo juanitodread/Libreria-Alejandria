dc84fc491c06136ee48f37e8d78bd802260d79d8
Marcela Sena <marce@jvan.vaio>
2012-04-21 16:10:21 -0500

    Modficacion para merge

---
+++ Alejandria/src/org/alejandria/web/admin/usuario/UsuarioController.java
@@ -0,0 +1,23 @@
+package org.alejandria.web.admin.usuario;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: marce
+ * Date: 4/20/12
+ * Time: 9:49 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+@Controller
+public class UsuarioController{
+
+    @RequestMapping("/usuario.run")
+    public ModelAndView getUsuarioView(ModelAndView mv){
+        mv.setViewName("org/alejandria/web/admin/usuario/usuario.admin");
+        return mv;
+    }
+}
