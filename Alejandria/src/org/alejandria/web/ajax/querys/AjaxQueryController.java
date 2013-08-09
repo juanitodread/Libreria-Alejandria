a8f19451ee4338819341104b625f4b7824197aae
juanitodread <juanitodread@gmail.com>
2013-08-09 00:13:30 -0500

    New commit

--- Alejandria/src/org/alejandria/web/ajax/querys/AjaxQueryController.java
+++ Alejandria/src/org/alejandria/web/ajax/querys/AjaxQueryController.java
@@ -14,8 +14,10 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import java.util.List;
 
 /**
- * Created with IntelliJ IDEA. User: Juan Date: 2/11/12 Time: 10:17 PM To change
- * this template use File | Settings | File Templates.
+ * Created with IntelliJ IDEA. 
+ * User: Juan 
+ * Date: 2/11/12 Time: 10:17 
+ * PM To change this template use File | Settings | File Templates.
  */
 @Controller
 public class AjaxQueryController {
